fcb2f7b0cd7562f3f91bc34cad01c6967016067b
CMD <156555638+CMDW01F@users.noreply.github.com>
2024-04-20 21:37:58 +0200

    Push

--- Password Protection/Protect.lua
+++ Password Protection/Protect.lua
@@ -4,5 +4,5 @@ while true do
     if pwd == "let me in" then break end
     print("Incorrect password, try again.")
   end
-  setAnalogOutput("back", 15)
+  setAnalogueOutput("back", 15)
   sleep(10.0)
